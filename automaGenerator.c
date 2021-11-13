83fafe6b8cf08c0723a17f0151a4cdb371f16a5e
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2021-11-13 12:17:57 +0100

    struct lr0_item

--- automaGenerator.c
+++ automaGenerator.c
@@ -2,8 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_GRAMMAR_PRODUCTIONS_NUMBER 20
+#define MAX_PRODUCTION_BODY_LENGTH 30
+
+typedef enum { false, true } bool;
+
+
+struct lr0_item {
+    char driver;
+    char body[MAX_PRODUCTION_BODY_LENGTH];
+
+    int marker_position;
+    bool marked;
+    int production_id; // utilizzato per controllare velocemente se una produzione e' uguale ad un altra senza controllare body e driver
+};
+
 
 int main(int argc, char** argv){
+    
+    struct lr0_item grammar[MAX_GRAMMAR_PRODUCTIONS_NUMBER];
+    char startSymbol;
+
+    char production[MAX_PRODUCTION_BODY_LENGTH + 10];
+    int productions_count = 0;
+    
+    if (argc != 2) {
+        printf("Use %s <start_symbol>\n", argv[0]);
+        exit(0);
+    }
+    startSymbol = argv[1][0];
+
+    // estendi la grammatica P a P' : grammatica con aggiunta la produzione S -> startSymbol
+    char fresh_production[] = "K -> _";
+    fresh_production[5] = startSymbol;
     
     return(0);
 }
